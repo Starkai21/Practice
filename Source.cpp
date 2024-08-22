1e09f0a78f7e0c78ea5f75e99541fed53770c592
Po Kya <pkya@atu.edu>
2024-08-22 17:06:03 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+// Programmer - Po Kya
+// Program -  Practice moving from vb to github
+// Date - 8/22/24
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "hello world";
+
+}
